910e8a397a4337580133001a069de2b2447bff42
rodion20198 <53440858+rodion20198@users.noreply.github.com>
2021-04-03 03:46:42 +0700

    Updates

---
+++ precache-manifest.0aabe66c65b72800c2c3495d7836b548.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea5f2d3d6d05579ed6c8",
+    "url": "./static/css/main.f585998a.chunk.css"
+  },
+  {
+    "revision": "ea5f2d3d6d05579ed6c8",
+    "url": "./static/js/main.7f23cc53.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d1acb2177a5b828c8a3b",
+    "url": "./static/css/2.cc2d577b.chunk.css"
+  },
+  {
+    "revision": "d1acb2177a5b828c8a3b",
+    "url": "./static/js/2.03741af4.chunk.js"
+  },
+  {
+    "revision": "c53e482c55280e18695f533a6fe47bee",
+    "url": "./static/media/coin.c53e482c.png"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "d8dde2c45fabb1c7700b35203f77be7a",
+    "url": "./static/media/click.d8dde2c4.png"
+  },
+  {
+    "revision": "9ee1976513b6b0d45d501b681b52fdd4",
+    "url": "./index.html"
+  }
+];
